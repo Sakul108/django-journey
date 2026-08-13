f8f1330505bbf8768604191707a6144cb2ac6834
Sakul <sakulkhatri108@gmail.com>
2026-08-13 11:23:20 +0545

    Resolved merge conflicts with main

--- config/product/views.py
+++ config/product/views.py
@@ -34,7 +34,6 @@ def details(request,pk):
     return render(request,'product/details.html',context)
 
 
-<<<<<<< HEAD
 def departments(request):
     departments = Department.objects.all()
     return render(request, 'product/departments.html', {'departments': departments})
@@ -43,5 +42,3 @@ def departments(request):
 def department_detail(request, slug):
     department = get_object_or_404(Department, slug=slug)
     return render(request, 'product/department_detail.html', {'department': department})
-=======
->>>>>>> main
